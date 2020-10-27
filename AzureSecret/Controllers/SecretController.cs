e23151ce09cb786652dd9bacc9101decc4741fc1
mohammad-hafijur <mohammad-hafijur.rahman@bostad.uppsala.se>
2020-10-27 11:31:38 +0100

    adding a api app project to demonstrate reading a secret from Azure KeyVault

---
+++ AzureSecret/Controllers/SecretController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AzureSecret.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SecretController : ControllerBase
+    {
+        private const string keyVaultURI = "your-keyvalut-uri";
+        private const string secretName = "your-secret-name";
+        private readonly ILogger<SecretController> _logger;
+
+        public SecretController(ILogger<SecretController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            var client = new SecretClient(new Uri(keyVaultURI), new DefaultAzureCredential());
+            var secret = await client.GetSecretAsync(secretName);
+            return secret.Value.Value;
+        }
+    }
+}
